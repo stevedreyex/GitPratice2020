2a45908d62a411190859dcee7cc965030a2035a7
vincent930093 <vincent930093@gmail.com>
2020-03-04 12:43:43 +0800

    practice7乙

--- GitPractice2020/GitPractice2020/GitPractice2020.cpp
+++ GitPractice2020/GitPractice2020/GitPractice2020.cpp
@@ -3,12 +3,18 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number == target)return true;
+	return false;
 }
 int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 - 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
